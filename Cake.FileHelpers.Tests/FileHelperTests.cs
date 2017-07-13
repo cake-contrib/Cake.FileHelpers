51ad1efafdf910ae8abdf5b153f490a8a710f782
Paul Voelker <paul.voelker@revealservices.com>
2017-07-13 10:44:20 -0500

    Added support for encoding parameters on functions.  Also created test cases for UTF8 encoding

--- Cake.FileHelpers.Tests/FileHelperTests.cs
+++ Cake.FileHelpers.Tests/FileHelperTests.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Text;
 using Cake.Xamarin.Tests.Fakes;
 using Cake.Core.IO;
 
@@ -50,6 +51,19 @@ namespace Cake.FileHelpers.Tests
         }
 
         [Test]
+        public void TestWriteAndReadTextWithUTF8Encoding ()
+        {
+            const string file = "./testdata/Text.txt";
+            const string contents = "This is a test. This is a smiley face: ☺";
+
+            context.CakeContext.FileWriteText (file, Encoding.UTF8, contents);
+
+            var read = context.CakeContext.FileReadText (file, Encoding.UTF8);
+
+            Assert.AreEqual (contents, read);
+        }
+
+        [Test]
         public void TestWriteAndReadLines ()
         {   
             const string file = "./testdata/Lines.txt";
@@ -58,6 +72,23 @@ namespace Cake.FileHelpers.Tests
             context.CakeContext.FileWriteLines (file, contents);
 
             var read = context.CakeContext.FileReadLines (file);
+
+            Assert.IsNotNull (read);
+            Assert.AreEqual (contents.Length, read.Length);
+
+            for (int i = 0; i < read.Length; i++)
+                Assert.AreEqual (contents[i], read[i]);
+        }
+
+        [Test]
+        public void TestWriteAndReadLinesWithUTF8Encoding ()
+        {
+            const string file = "./testdata/Lines.txt";
+            var contents = new[] { "This is a smiley face: ☺", "This is a copyright symbol: ©", "This is a recycle symbol: ♻" };
+
+            context.CakeContext.FileWriteLines (file, Encoding.UTF8, contents);
+
+            var read = context.CakeContext.FileReadLines (file, Encoding.UTF8);
 
             Assert.IsNotNull (read);
             Assert.AreEqual (contents.Length, read.Length);
@@ -78,6 +109,17 @@ namespace Cake.FileHelpers.Tests
         }
 
         [Test]
+        public void FindTextInFilesWithUTF8Encoding ()
+        {
+            SetupFilesUTF8 ();
+
+            var files = context.CakeContext.FindTextInFiles ("./testdata/*.txt", Encoding.UTF8, "Monkey");
+
+            Assert.IsNotNull (files);
+            Assert.AreEqual (1, files.Length);
+        }
+
+        [Test]
         public void FindRegexInFiles ()
         {
             SetupFiles ();
@@ -88,6 +130,16 @@ namespace Cake.FileHelpers.Tests
             Assert.AreEqual (1, files.Length);
         }
 
+        [Test]
+        public void FindRegexInFilesWithUTF8Encoding ()
+        {
+            SetupFilesUTF8 ();
+
+            var files = context.CakeContext.FindRegexInFiles ("./testdata/*.txt", Encoding.UTF8, @"\s{1}Monkey\s{1,}");
+
+            Assert.IsNotNull (files);
+            Assert.AreEqual (1, files.Length);
+        }
 
         [Test]
         public void ReplaceTextInFiles ()
@@ -102,6 +154,23 @@ namespace Cake.FileHelpers.Tests
             foreach (var f in files) {
                 var contents = context.CakeContext.FileReadText (f);
                 Assert.AreEqual (string.Format (PATTERN_FILE_BASE_VALUE, "Tamarin"), contents);
+            }
+        }
+
+        [Test]
+        public void ReplaceTextInFilesWithUTF8Encoding ()
+        {
+            SetupFilesUTF8 ();
+
+            var files = context.CakeContext.ReplaceTextInFiles ("./testdata/*.txt", Encoding.UTF8, "Monkey", "Tamarin");
+
+            Assert.IsNotNull (files);
+            Assert.AreEqual (1, files.Length);
+
+            foreach (var f in files)
+            {
+                var contents = context.CakeContext.FileReadText (f, Encoding.UTF8);
+                Assert.AreEqual (string.Format(PATTERN_FILE_BASE_VALUE, "Tamarin"), contents);
             }
         }
 
@@ -121,6 +190,23 @@ namespace Cake.FileHelpers.Tests
             }
         }
 
+        [Test]
+        public void ReplaceRegexInFilesWithUTF8Encoding ()
+        {
+            SetupFilesUTF8 ();
+
+            var files = context.CakeContext.ReplaceRegexInFiles ("./testdata/*.txt", Encoding.UTF8, @"\s{1}Monkey\s{1,}", " Tamarin ");
+
+            Assert.IsNotNull (files);
+            Assert.AreEqual (1, files.Length);
+
+            foreach (var f in files)
+            {
+                var contents = context.CakeContext.FileReadText (f, Encoding.UTF8);
+                Assert.AreEqual (string.Format(PATTERN_FILE_BASE_VALUE, "Tamarin"), contents);
+            }
+        }
+
 
         public const string PATTERN_FILE_BASE_VALUE = "The {0} makes great software.\nThis is not a surprise.";
 
@@ -131,6 +217,18 @@ namespace Cake.FileHelpers.Tests
                 context.CakeContext.FileWriteText (
                     string.Format ("./testdata/{0}.txt", i),
                     string.Format (PATTERN_FILE_BASE_VALUE, i == 2 ? "Monkey" : "Ape"));                
+            }
+        }
+
+        void SetupFilesUTF8()
+        {
+            // Setup files
+            for (int i = 1; i < 5; i++)
+            {
+                context.CakeContext.FileWriteText (
+                    string.Format("./testdata/{0}.txt", i),
+                    Encoding.UTF8,
+                    string.Format(PATTERN_FILE_BASE_VALUE, i == 2 ? "Monkey" : "Ape"));
             }
         }
     }
--- Cake.FileHelpers/FileHelpers.cs
+++ Cake.FileHelpers/FileHelpers.cs
@@ -3,6 +3,7 @@ using Cake.Core;
 using Cake.Core.IO;
 using System.IO;
 using System.Threading.Tasks;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
@@ -32,6 +33,21 @@ namespace Cake.FileHelpers
         }
 
         /// <summary>
+        /// Reads all text from a file
+        /// </summary>
+        /// <returns>The file's text.</returns>
+        /// <param name="context">The context.</param>
+        /// <param name="file">The file to read.</param>
+        /// <param name="encoding">The encoding to use for the file.</param>
+        [CakeMethodAlias]
+        public static string FileReadText(this ICakeContext context, FilePath file, Encoding encoding)
+        {
+            var filename = file.MakeAbsolute(context.Environment).FullPath;
+
+            return File.ReadAllText(filename, encoding);
+        }
+
+        /// <summary>
         /// Reads all lines from a file
         /// </summary>
         /// <returns>The file's text lines.</returns>
@@ -43,6 +59,21 @@ namespace Cake.FileHelpers
             var filename = file.MakeAbsolute (context.Environment).FullPath;
 
             return File.ReadAllLines (filename);
+        }
+
+        /// <summary>
+        /// Reads all lines from a file
+        /// </summary>
+        /// <returns>The file's text lines.</returns>
+        /// <param name="context">The context.</param>
+        /// <param name="file">The file to read.</param>
+        /// <param name="encoding">The encoding to use for the file.</param>
+        [CakeMethodAlias]
+        public static string[] FileReadLines(this ICakeContext context, FilePath file, Encoding encoding)
+        {
+            var filename = file.MakeAbsolute(context.Environment).FullPath;
+
+            return File.ReadAllLines(filename, encoding);
         }
 
         /// <summary>
@@ -60,6 +91,21 @@ namespace Cake.FileHelpers
         }
 
         /// <summary>
+        /// Writes all text to a file
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="file">The file to write to.</param>
+        /// <param name="encoding">The encoding to use for the file.</param>
+        /// <param name="text">The text to write.</param>
+        [CakeMethodAlias]
+        public static void FileWriteText(this ICakeContext context, FilePath file, Encoding encoding, string text)
+        {
+            var filename = file.MakeAbsolute(context.Environment).FullPath;
+
+            File.WriteAllText(filename, text, encoding);
+        }
+
+        /// <summary>
         /// Writes all text lines to a file
         /// </summary>
         /// <param name="context">The context.</param>
@@ -71,6 +117,21 @@ namespace Cake.FileHelpers
             var filename = file.MakeAbsolute (context.Environment).FullPath;
 
             File.WriteAllLines (filename, lines);
+        }
+
+        /// <summary>
+        /// Writes all text lines to a file
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="file">The file to write to.</param>
+        /// <param name="encoding">The encoding to use for the file.</param>
+        /// <param name="lines">The text lines to write.</param>
+        [CakeMethodAlias]
+        public static void FileWriteLines(this ICakeContext context, FilePath file, Encoding encoding, string[] lines)
+        {
+            var filename = file.MakeAbsolute(context.Environment).FullPath;
+
+            File.WriteAllLines(filename, lines, encoding);
         }
 
         /// <summary>
@@ -88,6 +149,21 @@ namespace Cake.FileHelpers
         }
 
         /// <summary>
+        /// Appends all text to a file
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="file">The file to append text to.</param>
+        /// <param name="encoding">The encoding to use for the file.</param>
+        /// <param name="text">The text to append.</param>
+        [CakeMethodAlias]
+        public static void FileAppendText(this ICakeContext context, FilePath file, Encoding encoding, string text)
+        {
+            var filename = file.MakeAbsolute(context.Environment).FullPath;
+
+            File.AppendAllText(filename, text, encoding);
+        }
+        
+        /// <summary>
         /// Appends all text lines to a file
         /// </summary>
         /// <param name="context">The context.</param>
@@ -99,6 +175,21 @@ namespace Cake.FileHelpers
             var filename = file.MakeAbsolute (context.Environment).FullPath;
 
             File.AppendAllLines (filename, lines);
+        }
+
+        /// <summary>
+        /// Appends all text lines to a file
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="file">The file to append text to.</param>
+        /// <param name="encoding">The encoding to use for the file.</param>
+        /// <param name="lines">The text lines to append.</param>
+        [CakeMethodAlias]
+        public static void FileAppendLines(this ICakeContext context, FilePath file, Encoding encoding, string[] lines)
+        {
+            var filename = file.MakeAbsolute(context.Environment).FullPath;
+
+            File.AppendAllLines(filename, lines, encoding);
         }
 
         /// <summary>
@@ -144,6 +235,37 @@ namespace Cake.FileHelpers
         }
 
         /// <summary>
+        /// Replaces the text in files matched by the given globber pattern
+        /// </summary>
+        /// <returns>The files that had text replaced in them.</returns>
+        /// <param name="context">The context.</param>
+        /// <param name="globberPattern">The globber pattern to match files to replace text in.</param>
+        /// <param name="encoding">The encoding to use for the file.</param>
+        /// <param name="findText">The text to find.</param>
+        /// <param name="replaceText">The replacement text.</param>
+        [CakeMethodAlias]
+        public static FilePath[] ReplaceTextInFiles(this ICakeContext context, string globberPattern, Encoding encoding, string findText, string replaceText)
+        {
+            var files = context.Globber.GetFiles(globberPattern);
+
+            var results = new ConcurrentBag<FilePath>();
+
+            Parallel.ForEach(files, f => {
+                var contents = FileReadText(context, f, encoding);
+
+                if (contents.Contains(findText))
+                {
+                    contents = contents.Replace(findText, replaceText);
+                    FileWriteText(context, f, encoding, contents);
+
+                    results.Add(f);
+                }
+            });
+
+            return results.ToArray();
+        }
+
+        /// <summary>
         /// Replaces the regex pattern in files matched by the given globber pattern.
         /// </summary>
         /// <returns>The files that had text replaced in them.</returns>
@@ -155,6 +277,21 @@ namespace Cake.FileHelpers
         public static FilePath[] ReplaceRegexInFiles (this ICakeContext context, string globberPattern, string rxFindPattern, string replaceText)
         {
             return ReplaceRegexInFiles (context, globberPattern, rxFindPattern, replaceText, RegexOptions.None);
+        }
+
+        /// <summary>
+        /// Replaces the regex pattern in files matched by the given globber pattern.
+        /// </summary>
+        /// <returns>The files that had text replaced in them.</returns>
+        /// <param name="context">The context.</param>
+        /// <param name="globberPattern">The globber pattern to match files to replace text in.</param>
+        /// <param name="encoding">The encoding to use for the file.</param>/// 
+        /// <param name="rxFindPattern">The regular expression to find.</param>
+        /// <param name="replaceText">The replacement text.</param>
+        [CakeMethodAlias]
+        public static FilePath[] ReplaceRegexInFiles(this ICakeContext context, string globberPattern, Encoding encoding, string rxFindPattern, string replaceText)
+        {
+            return ReplaceRegexInFiles(context, globberPattern, encoding, rxFindPattern, replaceText, RegexOptions.None);
         }
 
         /// <summary>
@@ -187,6 +324,37 @@ namespace Cake.FileHelpers
         }
 
         /// <summary>
+        /// Replaces the regex pattern in files matched by the given globber pattern.
+        /// </summary>
+        /// <returns>The files that had text replaced in them.</returns>
+        /// <param name="context">The context.</param>
+        /// <param name="globberPattern">The globber pattern to match files to replace text in.</param>
+        /// <param name="encoding">The encoding to use for the file.</param>
+        /// <param name="rxFindPattern">The regular expression to find.</param>
+        /// <param name="replaceText">The replacement text.</param>
+        /// <param name="rxOptions">The regular expression options to use.</param>
+        [CakeMethodAlias]
+        public static FilePath[] ReplaceRegexInFiles(this ICakeContext context, string globberPattern, Encoding encoding, string rxFindPattern, string replaceText, RegexOptions rxOptions)
+        {
+            var rx = new Regex(rxFindPattern, rxOptions);
+            var files = context.Globber.GetFiles(globberPattern);
+
+            var results = new ConcurrentBag<FilePath>();
+
+            Parallel.ForEach(files, f => {
+                var contents = FileReadText(context, f, encoding);
+                if (rx.IsMatch(contents))
+                {
+                    contents = rx.Replace(contents, replaceText);
+                    FileWriteText(context, f, encoding, contents);
+                    results.Add(f);
+                }
+            });
+
+            return results.ToArray();
+        }
+
+        /// <summary>
         /// Finds files with regular expression pattern in files matching the given globber pattern.
         /// </summary>
         /// <returns>The files which match the regular expression and globber pattern.</returns>
@@ -197,6 +365,20 @@ namespace Cake.FileHelpers
         public static FilePath[] FindRegexInFiles (this ICakeContext context, string globberPattern, string rxFindPattern)
         {
             return FindRegexInFiles (context, globberPattern, rxFindPattern, RegexOptions.None);
+        }
+
+        /// <summary>
+        /// Finds files with regular expression pattern in files matching the given globber pattern.
+        /// </summary>
+        /// <returns>The files which match the regular expression and globber pattern.</returns>
+        /// <param name="context">The context.</param>
+        /// <param name="globberPattern">The globber pattern to match files to replace text in.</param>
+        /// <param name="encoding">The encoding to use for the file.</param>/// 
+        /// <param name="rxFindPattern">The regular expression to find.</param>
+        [CakeMethodAlias]
+        public static FilePath[] FindRegexInFiles(this ICakeContext context, string globberPattern, Encoding encoding, string rxFindPattern)
+        {
+            return FindRegexInFiles(context, globberPattern, encoding, rxFindPattern, RegexOptions.None);
         }
 
         /// <summary>
@@ -225,6 +407,32 @@ namespace Cake.FileHelpers
         }
 
         /// <summary>
+        /// Finds files with regular expression pattern in files matching the given globber pattern.
+        /// </summary>
+        /// <returns>The files which match the regular expression and globber pattern.</returns>
+        /// <param name="context">The context.</param>
+        /// <param name="globberPattern">The globber pattern to match files to replace text in.</param>
+        /// <param name="encoding">The encoding to use for the file.</param> 
+        /// <param name="rxFindPattern">The regular expression to find.</param>
+        /// <param name="rxOptions">The regular expression options to use.</param>
+        [CakeMethodAlias]
+        public static FilePath[] FindRegexInFiles(this ICakeContext context, string globberPattern, Encoding encoding, string rxFindPattern, RegexOptions rxOptions)
+        {
+            var rx = new Regex(rxFindPattern, rxOptions);
+            var files = context.Globber.GetFiles(globberPattern);
+
+            var results = new ConcurrentBag<FilePath>();
+
+            Parallel.ForEach(files, f => {
+                var contents = FileReadText(context, f, encoding);
+                if (rx.IsMatch(contents))
+                    results.Add(f);
+            });
+
+            return results.ToArray();
+        }
+
+        /// <summary>
         /// Finds files with the given text in files matching the given globber pattern.
         /// </summary>
         /// <returns>The files which match the regular expression and globber pattern.</returns>
@@ -245,6 +453,30 @@ namespace Cake.FileHelpers
             });
 
             return results.ToArray ();
+        }
+
+        /// <summary>
+        /// Finds files with the given text in files matching the given globber pattern.
+        /// </summary>
+        /// <returns>The files which match the regular expression and globber pattern.</returns>
+        /// <param name="context">The context.</param>
+        /// <param name="globberPattern">The globber pattern to match files to replace text in.</param>
+        /// <param name="encoding">The encoding to use for the file.</param>
+        /// <param name="findPattern">The regular expression to find.</param>
+        [CakeMethodAlias]
+        public static FilePath[] FindTextInFiles(this ICakeContext context, string globberPattern, Encoding encoding, string findPattern)
+        {
+            var files = context.Globber.GetFiles(globberPattern);
+
+            var results = new ConcurrentBag<FilePath>();
+
+            Parallel.ForEach(files, f => {
+                var contents = FileReadText(context, f, encoding);
+                if (contents.Contains(findPattern))
+                    results.Add(f);
+            });
+
+            return results.ToArray();
         }
 
         /// <summary>
@@ -275,6 +507,34 @@ namespace Cake.FileHelpers
         }
 
         /// <summary>
+        /// Finds the regex matches in a text file.
+        /// </summary>
+        /// <returns>The regex matches in file.</returns>
+        /// <param name="context">Context.</param>
+        /// <param name="file">The text file.</param>
+        /// <param name="encoding">The encoding to use for the file.</param>
+        /// <param name="rxFindPattern">The regex pattern to search for.</param>
+        /// <param name="rxOptions">The regex options.</param>
+        [CakeMethodAlias]
+        public static List<string> FindRegexMatchesInFile(this ICakeContext context, FilePath file, Encoding encoding, string rxFindPattern, RegexOptions rxOptions)
+        {
+            if (!context.FileSystem.Exist(file))
+                return null;
+
+            var rx = new Regex(rxFindPattern, rxOptions);
+            var contents = FileReadText(context, file, encoding);
+
+            var values = new List<string>();
+
+            var matches = rx.Matches(contents);
+            foreach (Match m in matches)
+                if (m.Success && m.Value != null)
+                    values.Add(m.Value);
+
+            return values;
+        }
+
+        /// <summary>
         /// Finds the first regex match in a textfile.
         /// </summary>
         /// <returns>The first regex match in the file.</returns>
@@ -289,6 +549,26 @@ namespace Cake.FileHelpers
 
             if (values != null)
                 return values.FirstOrDefault ();
+
+            return null;
+        }
+
+        /// <summary>
+        /// Finds the first regex match in a textfile.
+        /// </summary>
+        /// <returns>The first regex match in the file.</returns>
+        /// <param name="context">The context.</param>
+        /// <param name="file">The file.</param>
+        /// <param name="encoding">The encoding to use for the file.</param>
+        /// <param name="rxFindPattern">The regex pattern to search for.</param>
+        /// <param name="rxOptions">The regex options.</param>
+        [CakeMethodAlias]
+        public static string FindRegexMatchInFile(this ICakeContext context, FilePath file, Encoding encoding, string rxFindPattern, RegexOptions rxOptions)
+        {
+            var values = FindRegexMatchesInFile(context, file, encoding, rxFindPattern, rxOptions);
+
+            if (values != null)
+                return values.FirstOrDefault();
 
             return null;
         }

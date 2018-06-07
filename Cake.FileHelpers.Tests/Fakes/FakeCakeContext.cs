815c02bd3b5bf55fd3bf12b8247e670b63f6b02e
Jamie Clayton <jamie.clayton@jenasysdesign.com.au>
2018-06-07 15:46:52 +1000

    Update the nuget package references to cake to Close #22
    * Bump the Cake reference from 0.26.1 to 0.28.0
    * Add Cake data context service into the unit tests. https://github.com/cake-build/cake/issues/2008

--- Cake.FileHelpers.Tests/Fakes/FakeCakeContext.cs
+++ Cake.FileHelpers.Tests/Fakes/FakeCakeContext.cs
@@ -3,6 +3,7 @@ using Cake.Core.IO;
 using Cake.Core;
 using Cake.Core.Tooling;
 using Cake.Testing;
+using Cake.FileHelpers.Tests.Fakes;
 
 namespace Cake.Xamarin.Tests.Fakes
 {
@@ -12,46 +13,50 @@ namespace Cake.Xamarin.Tests.Fakes
         readonly FakeLog log;
         readonly DirectoryPath testsDir;
 
-        public FakeCakeContext ()
+
+        public FakeCakeContext()
         {
-            testsDir = new DirectoryPath (
-                System.IO.Path.GetFullPath (AppContext.BaseDirectory));
-			
-            var fileSystem = new FileSystem ();
+            testsDir = new DirectoryPath(
+                System.IO.Path.GetFullPath(AppContext.BaseDirectory));
+
+            var fileSystem = new FileSystem();
             log = new FakeLog();
             var runtime = new CakeRuntime();
             var platform = new FakePlatform(PlatformFamily.Windows);
-            var environment = new CakeEnvironment (platform, runtime, log);
-            var globber = new Globber (fileSystem, environment);
-            
-            var args = new FakeCakeArguments ();
-            var processRunner = new ProcessRunner (environment, log);
-            var registry = new WindowsRegistry ();
+            var environment = new CakeEnvironment(platform, runtime, log);
+            var globber = new Globber(fileSystem, environment);
+
+            var args = new FakeCakeArguments();
+            var processRunner = new ProcessRunner(environment, log);
+            var registry = new WindowsRegistry();
 
             var toolRepository = new ToolRepository(environment);
             var toolResolutionStrategy = new ToolResolutionStrategy(fileSystem, environment, globber, new FakeConfiguration());
             IToolLocator tools = new ToolLocator(environment, toolRepository, toolResolutionStrategy);
-            context = new CakeContext (fileSystem, environment, globber, log, args, processRunner, registry, tools);
+            ICakeDataService data = new FakeDataService(); 
+            context = new CakeContext(fileSystem, environment, globber, log, args, processRunner, registry, tools, data);
             context.Environment.WorkingDirectory = testsDir;
         }
 
-        public DirectoryPath WorkingDirectory {
+        public DirectoryPath WorkingDirectory
+        {
             get { return testsDir; }
         }
 
-        public ICakeContext CakeContext {
+        public ICakeContext CakeContext
+        {
             get { return context; }
         }
 
-        public string GetLogs ()
+        public string GetLogs()
         {
             return string.Join(Environment.NewLine, log.Messages);
         }
 
-        public void DumpLogs ()
+        public void DumpLogs()
         {
             foreach (var m in log.Messages)
-                Console.WriteLine (m);
+                Console.WriteLine(m);
         }
     }
 }
---
+++ Cake.FileHelpers.Tests/Fakes/FakeDataService.cs
@@ -0,0 +1,20 @@
+﻿using Cake.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cake.FileHelpers.Tests.Fakes
+{
+    class FakeDataService : ICakeDataService
+    {
+        public void Add<TData>(TData value) where TData : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public TData Get<TData>() where TData : class
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
